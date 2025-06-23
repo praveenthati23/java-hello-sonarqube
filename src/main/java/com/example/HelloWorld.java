a46749e7015153a02e328ec8b10ee802a2108b54
cvr-devops <venkat.chittipeddi01@gmail.com>
2025-06-23 16:48:31 +0530

    Initial commit

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // BAD PRACTICE: hardcoded credentials
+        String password = "123456";
+        String username = "admin";
+    }
+}
+
